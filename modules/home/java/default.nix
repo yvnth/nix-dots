cf7c0f717f79f2fa967c0f677419b395cb86dbe0
yvnth <yvnth@users.noreply.github.com>
2026-06-25 22:12:14 +0530

    chore(java): format stuff

--- modules/home/java/default.nix
+++ modules/home/java/default.nix
@@ -4,6 +4,7 @@
   pkgs,
   ...
 }:
+
 {
   options.homeModules.java.enable = lib.mkEnableOption "java";
 
@@ -15,12 +16,16 @@
       maven
     ];
 
-    home.sessionVariables.JAVA_HOME = "${pkgs.jdk21}";
+    home.sessionVariables = {
+      JAVA_HOME = "${pkgs.jdk21}/lib/openjdk";
+    };
 
-    home.file.".jdks/jdk8".source = "${pkgs.jdk8}/lib/openjdk";
-    home.file.".jdks/jdk11".source = "${pkgs.jdk11}/lib/openjdk";
-    home.file.".jdks/jdk17".source = "${pkgs.jdk17}/lib/openjdk";
-    home.file.".jdks/jdk21".source = "${pkgs.jdk21}/lib/openjdk";
-    home.file.".jdks/jdk25".source = "${pkgs.jdk25}/lib/openjdk";
+    home.file = {
+      ".jdks/jdk8".source = "${pkgs.jdk8}/lib/openjdk";
+      ".jdks/jdk11".source = "${pkgs.jdk11}/lib/openjdk";
+      ".jdks/jdk17".source = "${pkgs.jdk17}/lib/openjdk";
+      ".jdks/jdk21".source = "${pkgs.jdk21}/lib/openjdk";
+      ".jdks/jdk25".source = "${pkgs.jdk25}/lib/openjdk";
+    };
   };
 }
